f86dcd3f84433ee622e377b90ed7af1df32d989c
Ray Haleblian <ray@haleblian.com>
2014-09-06 13:25:41 -0700

    Add launcher for VS 2012.

---
+++ vs2012e_2015-x64.bat
@@ -0,0 +1,6 @@
+set MAYA_VERSION=2015
+set MAYA_PREFSVERSION=2015-x64
+set VC="C:\Program Files (x86)\Microsoft Visual Studio 11.0\Common7\IDE\WDExpress.exe" 
+
+set MAYA_LOCATION=D:\Programs\Autodesk\Maya%MAYA_VERSION%
+%VC% maya_nif_plugin.sln
